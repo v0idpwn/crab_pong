2cd7a8aac84d8871ceb20a822b35a99d4b492118
v0idpwn <v0idpwn@gmail.com>
2020-02-08 12:21:36 -0300

    Added a wall to avoid going out of screen
    Needed to implement copy for this purpose

--- src/bar.rs
+++ src/bar.rs
@@ -1,3 +1,4 @@
+#[derive(Copy, Clone)]
 pub struct Bar {
     pub pos_x: i32,
     pub pos_y: i32,
--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ use sdl2::pixels::Color;
 use sdl2::rect::Rect;
 use std::time::Duration;
 mod bar;
+use bar::Bar;
 
 fn main() {
     let sdl_context = sdl2::init().unwrap();
@@ -24,8 +25,8 @@ fn main() {
     canvas.present();
 
     let mut event_pump = sdl_context.event_pump().unwrap();
-    let mut bar1 = bar::Bar::new(20, 20);
-    let mut bar2 = bar::Bar::new(740, 20);
+    let mut bar1 = Bar::new(20, 20);
+    let mut bar2 = Bar::new(740, 20);
 
     'running: loop {
         for event in event_pump.poll_iter() {
@@ -33,19 +34,19 @@ fn main() {
                 Event::KeyDown {
                     keycode: Some(Keycode::S),
                     ..
-                } => bar1.mv(5),
+                } => if !is_at_bottom_border(bar1) {bar1.mv(5)} else {},
                 Event::KeyDown {
                     keycode: Some(Keycode::W),
                     ..
-                } => bar1.mv(-5),
+                } => if !is_at_top_border(bar1) {bar1.mv(-5)} else {},
                 Event::KeyDown {
                     keycode: Some(Keycode::Down),
                     ..
-                } => bar2.mv(5),
+                } => if !is_at_bottom_border(bar2) {bar2.mv(5)} else {},
                 Event::KeyDown {
                     keycode: Some(Keycode::Up),
                     ..
-                } => bar2.mv(-5),
+                } => if !is_at_top_border(bar2) {bar2.mv(-5)} else {},
                 Event::Quit { .. }
                 | Event::KeyDown {
                     keycode: Some(Keycode::Escape),
@@ -54,18 +55,30 @@ fn main() {
                 _ => {}
             }
         }
+
         canvas.set_draw_color(Color::RGB(0, 0, 0));
         canvas.clear();
 
         canvas.set_draw_color(Color::RGB(161, 209, 174));
+
         let rect = Rect::new(bar1.pos_x, bar1.pos_y, bar1.width, bar1.heigth);
         let rect2 = Rect::new(bar2.pos_x, bar2.pos_y, bar2.width, bar2.heigth);
+
         canvas.draw_rect(rect).unwrap();
         canvas.fill_rect(rect).unwrap();
         canvas.draw_rect(rect2).unwrap();
         canvas.fill_rect(rect2).unwrap();
-
         canvas.present();
+
         ::std::thread::sleep(Duration::new(0, 1000000000u32 / 60));
+    }
+
+    fn is_at_top_border(bar: Bar) -> bool{
+        bar.pos_y <= 20
+    }
+
+    // Problem: we are considering a hardcoded 160 height for the bars
+    fn is_at_bottom_border(bar: Bar) -> bool{
+        bar.pos_y >= 420
     }
 }
